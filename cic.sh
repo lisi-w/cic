bba0fdb81aaf68092c486ad1df8f5e40a68ec5f5
Elysia Witham <witham3@llnl.gov>
2021-08-27 15:30:05 -0700

    updated cron script

--- cic.sh
+++ cic.sh
@@ -2,11 +2,11 @@
 
 srcdir=`dirname $0`
 cd $srcdir
-
-bash /export/witham3/esgf/conda.sh
+ENV=$1
+bash ./conda.sh
 . ~/.bashrc
 
-conda activate cic
+conda activate $ENV
 
 thedate=`date +%y%m%d_%H%M`
 destpath=/p/user_pub/publish-queue/inconsistencies/$thedate
@@ -16,7 +16,7 @@ mkdir -p $destpath
 
 
 # nohup python3 cic.py --output-dir $destpath --cmor-tables $cmorpath --email e.witham@columbia.edu ames4@llnl.gov --fix-errors --enable-email > $destpath/cic.$thedate.log
-nohup python3 cic.py --output-dir $destpath/ --cmor-tables $cmorpath --email e.witham@columbia.edu ames4@llnl.gov --fix-errors --enable-ac > $destpath/cic.$thedate.log
+nohup python3 cic.py --output-dir $destpath/ --cmor-tables $cmorpath --email ames4@llnl.gov --fix-errors --enable-ac > $destpath/cic.$thedate.log
 cp $destpath/metrics.* ./metrics/
 mkdir $destpath/graphs
 python3 time_series.py $destpath/graphs
